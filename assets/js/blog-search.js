d5e40bd03b3eb9d202066e0b585627345e456a06
lislis <mail@lislis.de>
2018-11-07 14:31:17 +0100

    blog search \o/

--- assets/js/blog-search.js
+++ assets/js/blog-search.js
@@ -1,100 +1,96 @@
-// https://discourse.gohugo.io/t/live-hugo-site-search-with-lunr-js/2857
-
 document.addEventListener('DOMContentLoaded', function (event) {
   var searchOverlay = document.getElementById('js-blog-search-overlay');
   var searchInput = document.getElementById('js-blog-search-input');
   var closeSearch = document.getElementById('js-blog-search-clear');
   var searchResults = document.getElementById('js-search-results');
+  var searchTerm = document.getElementById('js-search-term');
+  var blogIndex = document.getElementById('js-blog-index');
+
+  searchInput.value = '';
 
   closeSearch.onclick = function () {
-    console.log('close click');
     if (searchOverlay.classList.contains('open')) {
       searchOverlay.classList.remove('open');
+      blogIndex.classList.remove('hidden');
+      searchTerm.innerHtml = '';
     }
   }
 
-  //for more information on lunr.js, go to http://lunrjs.com/
   window.searchData;
   searchInput.addEventListener('keyup', lunrSearch, true);
 
-  function indexJsonPath() {
-    return '/blog/index.json';
-  }
-
   var searchReq = new XMLHttpRequest();
 
-  searchReq.open('GET', indexJsonPath(), true);
-  searchReq.onload = function() {
+  searchReq.open('GET', '/blog/index.json', true);
+  searchReq.onload = function () {
     if (this.status >= 200 && this.status < 400) {
       console.log("Got blog search index");
       searchData = JSON.parse(this.response);
 
-      window.bidx = lunr(function() {
+      window.bidx = lunr(function () {
         this.field('id');
         this.field('url');
         this.field('title', { boost: 50 });
         this.field('tags', { boost: 10});
         this.field('content', { boost: 10 });
 
-        searchData.forEach(function(obj, index) {
+        searchData.forEach(function (obj, index) {
           obj.id = index;
           this.add(obj);
         }, this);
       });
 
     } else {
-      console.log("Failed status for json. Check network panel");
+      console.log("Failed status for blog search index. Check network panel");
     }
   };
-  searchReq.onerror = function() {
-    console.log("Error when attempting to load json.");
+  searchReq.onerror = function () {
+    console.log("Error when attempting to load blog search index.");
   };
   searchReq.send();
 
-  function lunrSearch(event) {
+  function lunrSearch (event) {
     var query = searchInput.value;
-    //var searchResults = document.querySelector('#search-results');
     if (query.length === 0) {
       searchResults.innerHTML = '';
-      console.log('query too short');
+      searchTerm.innerHTML = '';
       searchOverlay.classList.remove('open');
+      blogIndex.classList.remove('hidden');
     }
     if ((event.keyCode !== 9) && (query.length > 2)) {
+      searchTerm.innerHTML = query;
       searchOverlay.classList.add('open');
-      console.log('results found', window.bidx.search(query).length);
+      blogIndex.classList.add('hidden');
       var matches = window.bidx.search(query);
       displayResults(matches);
     }
   }
 
-  function displayResults(results) {
-    console.log(results);
+  function emptyResults (term) {
+    var out;
+    out = '<li><h2 class="h4">Keine Ergebnisse für "'+ term +'"</h2>';
+    out += '<p class="h4">Versuche einen anderen Suchbegriff</p></li>';
+    return out;
+  }
+
+  function displayResults (results) {
     var inputVal = searchInput.value;
     if (results.length) {
       searchResults.innerHTML = '';
+      var out = '';
       results.forEach(function(result) {
         var item = window.searchData[result.ref];
-        var appendString = '<li class="search-result">';
-        appendString += '<h4><a href="'+ item.url +'">'+ item.title +'</a></h4>';
-        if (item.section) {
-          var section = [item.section.split('')[0].toUpperCase(),
-                         item.section.split('').splice(1).join('')].join('');
-          appendString += '<p>In '+ section +'</p>';
-        }
-        if (item.tags) {
-          appendString += '<ul class="tags">'+ item.tags.map(x => x) +'</ul>';
-        }
-        if (item.summary) {
-          appendString += '<p>'+ item.summary +'</p>';
-        }
-        appendString += '</li>';
-        searchResults.innerHTML += appendString;
+        var date = new Date(item.date.replace(' +0000 UTC', 'Z'))
+            .toLocaleDateString('de-DE');
+        out += '<li class="c__post--item">';
+        out += '<a href="'+ item.url +'" class="row no-gutters">';
+        out += '<h2 class="c__post__title h4 col col-12 col-sm-10">'+ item.title +'</h2>';
+        out += '<p class="c__post__date h4 col-12 col-sm-2">'+ date +'</p>';
+        out += '</a></li>';
       });
+      searchResults.innerHTML += out;
     } else {
-      searchResults.innerHTML = `<li class=\"search-result none\">
-No results found for <span class=\"input-value\">${inputVal}</span>.<br/>
-Please check spelling and spacing.</li>`;
+      searchResults.innerHTML = emptyResults(inputVal);
     }
   }
-
 });
