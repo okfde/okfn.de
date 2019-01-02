1e7785453bd9e014499e7c48a268b8ab0573be34
lislis <mail@lislis.de>
2019-01-02 17:14:30 +0100

    top income sources

--- assets/js/viz.js
+++ assets/js/viz.js
@@ -1,10 +1,88 @@
 document.addEventListener('DOMContentLoaded', function (event) {
   incomeTypesViz();
+  incomesViz();
   expenseViz();
 });
 
+function incomesViz() {
+  var margin = {top:50, right:10, bottom:10, left:20};
+  var width = 800 - margin.left - margin.right;
+  var height = 350 - margin.top - margin.bottom;
+  var scaleWidth = 500;
+  var magicSpacing = 210;
+
+  var svg = d3.select('div.einnahmen').append('svg')
+      .attr('width' , width + margin.left + margin.right)
+      .attr('height' , height + margin.top + margin.bottom)
+      .attr("transform", "translate("+ margin.left +","+ margin.top +")");
+
+  d3.csv("/okf/finanzierung/einnahmen.csv").then(function(data) {
+    var sumAmount = data.reduce(function (sum, d) {
+      return sum + parseInt(d.amount, 10);
+    }, 0);
+    data = data.map(function (d) {
+      d.percentage = (d.amount / sumAmount * 100).toFixed(1);
+      return d;
+    });
+    data = data.sort((a, b) => b.amount - a.amount);
+    data = data.slice(0,9);
+
+    var xScale = d3.scaleLinear()
+        .domain([0, 50])
+        .range([0, scaleWidth]);
+    var xAxis = d3.axisBottom(xScale);
+    svg.append('g')
+      .attr("class", "x axis")
+      .attr("transform", "translate("+ magicSpacing +","+ height +")")
+      .call(xAxis);
+
+    var yScale = d3.scaleBand().rangeRound([0, height]);
+    var yAxis = d3.axisLeft(yScale);
+    yScale.domain(data.map(function(d) { return d.item; }));
+
+    var yAxis_g = svg.append("g")
+        .attr("class", "y axis")
+        .attr("transform", "translate(" + magicSpacing +",0)")
+        .call(yAxis)
+        .selectAll("text");
+
+    var tip = d3.tip()
+        .attr('class', 'd3-tip n')
+        .offset([-10, 0])
+        .html(function(d) {
+          return "<span>" + d.amount + " Euro ("+ d.percentage + "%) von " + d.item + " (" + d.category + ")</span>";
+        });
+
+    svg.call(tip);
+
+    var barHeight = 15;
+    svg.selectAll('rect.bar')
+      .data(data)
+      .enter()
+      .append('rect')
+      .attr('class', 'one-bar')
+      .attr('width', function (d, i) {
+        return xScale(d.percentage);
+      })
+      .attr('height', function (d, i) {
+        return barHeight;
+      })
+      .attr('x', function(d,i){
+        return magicSpacing;
+      })
+      .attr('y', function(d, i) {
+        return (i * (barHeight * 2.13) + (10));
+      })
+      .attr('fill', '#382eff')
+      .on('mouseover', tip.show)
+      .on('mouseout', tip.hide);
+  });
+}
+
+
+
 function incomeTypesViz() {
-  var margin = {top:30, right:10, bottom:90, left:20};
+  var margin = {top:20, right:10, bottom:50, left:20};
   var width = 900 - margin.left - margin.right;
   var height = 400 - margin.top - margin.bottom;
   var scaleWidth = 500;
@@ -25,7 +103,6 @@ function incomeTypesViz() {
     .call(xAxis);
 
   d3.csv("/okf/finanzierung/einnahmen-kategorien.csv").then(function(data) {
-
     var yScale = d3.scaleBand().rangeRound([0, height]);
     var yAxis = d3.axisLeft(yScale);
     yScale.domain(data.map(function(d) { return d.category; }));
@@ -61,7 +138,7 @@ function incomeTypesViz() {
         return magicSpacing;
       })
       .attr('y', function(d, i) {
-        return (i * (height/ data.length) + (barHeight / 2));;
+        return (i * (height/ data.length) + (barHeight / 2));
       })
       .attr('fill', '#382eff')
       .on('mouseover', tip.show)
@@ -70,7 +147,7 @@ function incomeTypesViz() {
 }
 
 function expenseViz() {
-  var margin = {top:10, right:10, bottom:90, left:20};
+  var margin = {top:10, right:10, bottom:50, left:20};
   var width = 800 - margin.left - margin.right;
   var height = 480 - margin.top - margin.bottom;
   var scaleWidth = 600;
@@ -91,7 +168,6 @@ function expenseViz() {
     .call(xAxis);
 
   d3.csv("/okf/finanzierung/ausgaben.csv").then(function(data) {
-
     var yScale = d3.scaleBand().rangeRound([0, height]);
     var yAxis = d3.axisLeft(yScale);
 
