8986776b98b293fc25ab1c0d804ddb1d8f5a0978
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2020-02-06 18:18:42 +0100

    Simplify build script

--- scripts/ci/build.sh
+++ scripts/ci/build.sh
@@ -4,8 +4,14 @@ set -ex
 
 hugo
 
-bash scripts/ci/build-site-search-index.sh
-bash scripts/ci/build-blog-search-index.sh
+cat public/index.json | \
+  node scripts/lunr/site-index.js > \
+  public/js/site-search-index.json
+
+cat public/blog/index.json | \
+    node scripts/lunr/blog-index.js > \
+         public/js/blog-search-index.json
+
 
 cp public/blog/atom.xml public/atom.xml
 mkdir public/feed/ && cp public/blog/rss.xml public/feed/index.xml
