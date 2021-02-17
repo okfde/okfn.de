00161df5aa3e9c0fbe8f2ccd64b0983ae7b4c0d5
Travis <travis@travis-ci.org>
2021-02-17 20:00:41 +0000

    Travis build 1380

---
+++ js/banner.js
@@ -0,0 +1,23 @@
+const BANNER_ID = 'zehn';
+const TARGET_PAGE = 'anniversary';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const element = document.querySelector('aside.dropbanner');
+
+  const onTarget = window.location.pathname.endsWith(`${TARGET_PAGE}/`);
+  const show = !onTarget && localStorage.getItem('hideDropbanner') !== BANNER_ID;
+  if (show && element) {
+    element.classList.remove('gone');
+  } else {
+    element.remove();
+  }
+
+  const hideBtn = document.querySelector('aside.dropbanner button');
+  if (hideBtn) {
+    hideBtn.addEventListener('click', () => {
+      element.classList.add('gone');
+      localStorage.setItem('hideDropbanner', BANNER_ID);
+      setTimeout(() => element.remove(), 1000);
+    });
+  }
+})
