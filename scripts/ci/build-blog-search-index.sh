b26d25423ae071ef4ce75eb63f1c0c8d710cb356
lislis <mail@lislis.de>
2021-08-03 14:12:32 +0200

    rm old ci scripts

--- scripts/ci/build-blog-search-index.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-cat $TRAVIS_BUILD_DIR/public/blog/index.json | \
-    node $TRAVIS_BUILD_DIR/scripts/lunr/blog-index.js > \
-         $TRAVIS_BUILD_DIR/public/js/blog-search-index.json
--- scripts/ci/build-site-search-index.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-cat $TRAVIS_BUILD_DIR/public/index.json | \
-  node $TRAVIS_BUILD_DIR/scripts/lunr/site-index.js > \
-  $TRAVIS_BUILD_DIR/public/js/site-search-index.json
