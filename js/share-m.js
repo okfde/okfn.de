44a1c29716c0e7b1f9a209321c9e3bad9a857797
maximusvo <maximilian-voigt@gmx.de>
2023-07-31 08:46:34 +0000

    Update release to output generated at 3cd8aff

---
+++ js/share-m.js
@@ -0,0 +1,19 @@
+/* Generate a share link for the user's Mastodon domain */
+function mastodonShare(e){
+
+    // Get the source text
+    var src = e.getAttribute("data-src");
+
+    // Get the Mastodon domain
+    domain = prompt("Enter your Mastodon domain", "mastodon.social");
+
+    if (domain == "" || domain == null){
+        return;
+    }
+
+    // Build the URL
+    url = "https://" + domain + "/share?text=" + src;
+
+    // Open a window on the share page
+    window.open(url, '_blank');
+}
